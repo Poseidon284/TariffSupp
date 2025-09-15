23882c455b72ede7924ab108d64d3d95a4a28a25
Radhesh Rathnam <131352235+Poseidon284@users.noreply.github.com>
2025-09-15 22:04:25 +0530

    Update RAG_utils.py

--- RAG_utils.py
+++ RAG_utils.py
@@ -179,7 +179,7 @@ def rag_answer(query, n_results=10):
     store = init_chroma()
     chunks = query_chroma(query, store, n_results=n_results)
     context = "\n\n".join([f"[{c['doc_type'].upper()} | {c['source']}] {c['text']}" for c in chunks])
-    context = "Columns in Table:{SNo,Article,Total Quantity,Total Sales,unit_price,First_Sale,Recent_Sale,Days_in_Market,Unique_purchase_dates,Per_day_Sale,Popularity_Score,Relevance_Score}"+context
+    context = f"Columns in Table:{columns}"+context
     prompt = ChatPromptTemplate.from_template(
         "You are a helpful assistant answering based on supplier and tariff documents.\n"
         "Query: {query}\n\n"
